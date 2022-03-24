a333a5705dbc8cf4f6aec9f9f8df4aa6d05c3e91
tiki <thiagoqua16@gmail.com>
2022-03-24 18:55:37 -0300

    Implementación de mensaje de saludo al establecer la conexion.

--- seqFTP/cliFTP.c
+++ seqFTP/cliFTP.c
@@ -3,25 +3,34 @@
 #include<arpa/inet.h>
 #include<netinet/in.h>
 #include<string.h>
+#include<unistd.h>
+
+#define BUFFLEN 30
 
 int main(int argc,char *args[]){
     if(argc != 3){
         printf("** cantidad de argumentos incorrecta **\n");
         exit(1);
     }
-    int fhc,port,saddrlen;
+    int fhs,port,saddrlen;
     struct sockaddr_in saddress;
+    char buffer[BUFFLEN] = {0};
     port = atoi(args[2]);
     saddrlen = sizeof(saddress);
     saddress.sin_family = AF_INET;
     saddress.sin_addr.s_addr = inet_addr(args[1]);
     saddress.sin_port = htons(port);
-    if((fhc = socket(AF_INET,SOCK_STREAM,0)) < 0){
+    if((fhs = socket(AF_INET,SOCK_STREAM,0)) < 0){
         printf("** fallo en la creacion del socket **\n");
         return -1;
     }
-    if(connect(fhc,(struct sockaddr*)&saddress,saddrlen) < 0){
+    if(connect(fhs,(struct sockaddr*)&saddress,saddrlen) < 0){
         printf("** fallo la conexion **\n");
         return -2;
     }
+    if(read(fhs,buffer,sizeof(buffer)) < 0){
+        printf("** fallo la recepcion del mensaje **\n");
+        return -3;
+    }
+    printf("message from server: %s\n",buffer);
 return 0;}
--- seqFTP/srvFTP.c
+++ seqFTP/srvFTP.c
@@ -2,8 +2,12 @@
 #include<stdlib.h>
 #include<arpa/inet.h>
 #include<netinet/in.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<errno.h>
 
-#define VERSION 1.0
+#define VERSION "1.0"
+#define BUFFLEN 30
 
 int main(int argc,char *args[]){
     if(argc != 2){
@@ -12,25 +16,31 @@ int main(int argc,char *args[]){
     }
     int fhs,fhc,addrlen,port;
     struct sockaddr_in address;
+    char buffer[BUFFLEN] = {0};
     port = atoi(args[1]);
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = inet_addr("127.0.0.25");
     address.sin_port = htons(port);
     addrlen = sizeof(address);
     if((fhs = socket(AF_INET,SOCK_STREAM,0)) < 0){
-        printf("** fallo en la creacion del socket **");
+        printf("** fallo en la creacion del socket **\n");
         return -1;
     }
     if(bind(fhs,(struct sockaddr*)&address,(socklen_t)addrlen) < 0){
-        printf("** fallo el bind **");
+        printf("** fallo el bind **\ncodigo de error %d.\n",errno);
         return -2;
     }
     if(listen(fhs,3) < 0){
-        printf("** fallo el listen **");
+        printf("** fallo el listen **\n");
         return -3;
     }
     if((fhc = accept(fhs,(struct sockaddr*)&address,(socklen_t*)&addrlen)) < 0){
-        printf("** fallo el accept **");
+        printf("** fallo el accept **\n");
         return -4;
+    }
+    sprintf(buffer,"220 %s version %s.",args[0],VERSION);
+    if(write(fhc,buffer,sizeof(buffer)) < 0){
+        printf("** fallo el enviado del mensaje **\n");
+        return -5;
     }
 return 0;}
